92dea65d80e35a31bfe196fd665d63df605f153c
asor585686 <91413982+asor585686@users.noreply.github.com>
2022-12-06 16:59:40 +0900

    Delete index.mjs

--- js/index.mjs
+++
@@ -1,7 +0,0 @@
-export { Animate, Timeout } from './Animate.mjs';
-export { Emitter } from './Emitter.mjs';
-export { Tick } from './Tick.mjs';
-export { Mouse } from './Mouse.mjs';
-export { DOM } from './dom.mjs';
-export { State } from './State.mjs';
-export { OMath, OArray, OString, OPromise } from './Extensions.mjs';
